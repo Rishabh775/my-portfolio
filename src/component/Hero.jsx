47416a0e4c23aed0a749b9cca84f9fe0194b2915
Rishabh Singh <rishabh797604@gmail.com>
2024-03-09 18:11:01 +0530

    Update Hero.jsx

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -19,7 +19,7 @@ export default function Hero() {
       initial={{ opacity: 0, y: -20 }}
       animate={{ opacity: 1, scale: 1, y: 0 }}
       transition={{ type: "spring", stiffness: 40, damping: 20 }}
-      className="  relative font-DM Sans h-[100vh] sm:h-[90vh] overflow-clip bg-black snap-center"
+      className="  relative font-DM Sans h-[110vh] sm:h-[90vh] overflow-clip bg-black snap-center"
       id="HOME"
     >
       <div className="grid grid-cols-1  lg:grid-cols-3 h-[80vh] gap-8 w-5/6 mx-auto">
